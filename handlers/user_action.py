24196f8e69ce28ead130a174ca812099082e36dd
Filin153 <dimons2006@yandex.ru>
2025-07-24 00:10:22 +0300

    fix ORM error

--- handlers/user_action.py
+++ handlers/user_action.py
@@ -9,6 +9,7 @@ from utils.to_ics import ToCalendar
 
 router = Router()
 
+
 @router.message()
 async def add_user_action(message: Message):
     user_a_create = UserActionCreateSchema(
@@ -28,30 +29,30 @@ async def add_user_action(message: Message):
         if len(last_user_action) > 0:
             last_user_action = last_user_action[0]
             await user_a_repo.set_stop(
-                session=session,
-                uuid=last_user_action.uuid,
-                stop=True
+                session=session, uuid=last_user_action.uuid, stop=True
             )
 
-        user_a_orm = await user_a_repo.add(
-            session=session,
-            action_data=user_a_create
-        )
+            await session.flush()
+
+            last_user_action = await user_a_repo.get_by_filter_from_end(
+                session=session,
+                limit=1,
+                user_tg_id=user_a_create.user_tg_id,
+            )
+            last_user_action = last_user_action[0]
+
+            calendar = ToCalendar()
+            await calendar.update_user_ics(
+                UserActionDTOSchema(
+                    user_tg_id=last_user_action.user_tg_id,
+                    action=last_user_action.action,
+                    create_at=last_user_action.create_at,
+                    update_at=last_user_action.update_at,
+                )
+            )
+
+        await user_a_repo.add(session=session, action_data=user_a_create)
 
         await session.commit()
 
-        calendar = ToCalendar()
-        await calendar.update_user_ics(UserActionDTOSchema(
-            user_tg_id=user_a_create.user_tg_id,
-            action=user_a_create.action,
-            create_at=user_a_create.create_at,
-            update_at=user_a_create.update_at,
-        ))
-
-    
     await message.reply("Успешно добавленно!")
-
-    
-
-
-
